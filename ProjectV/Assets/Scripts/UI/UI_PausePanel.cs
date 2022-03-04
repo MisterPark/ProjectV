b2935c1fb0c782fbc882200f8e2c8721b544b939
djdngus32 <djdngus32@naver.com>
2022-03-04 12:12:47 +0900

    정지 패널 만드는중

---
+++ ProjectV/Assets/Scripts/UI/UI_PausePanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_PausePanel : UI
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClickResume()
+    {
+
+    }
+
+    public void OnClickOption()
+    {
+
+    }
+}
