085e3fa60b122012321bbaccb694d563501c9273
djdngus32 <djdngus32@naver.com>
2022-03-05 16:35:25 +0900

    장비창 제작중

--- ProjectV/Assets/Scripts/UI/UI_Equipment.cs
+++ ProjectV/Assets/Scripts/UI/UI_Equipment.cs
@@ -7,30 +7,59 @@ public class UI_Equipment : MonoBehaviour
 
     private RectTransform rectTransform;
     [SerializeField] private RectTransform background;
-    public UI_SlotInfomation[] slot;
+    public UI_SlotInfomation[] activeSlot;
+    public UI_SlotInfomation[] passiveSlot;
 
     // Start is called before the first frame update
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        HideAllSlot();
     }
 
     public void SetSkillInfomations(List<Skill> skills)
     {
         int i = 0;
+        int j = 0;
         foreach (Skill skill in skills)
         {
-            //SkillData data = DataManager.Instance.skillDatas[(int)item.kind].skillData;
-            //slot.icon.sprite = data.icon;
-            //slot.text.text = "LV." + (item.nextLevel).ToString();
             SkillData data = skill.SkillData;
-            slot[i].icon.sprite = data.icon;
-            if (skill.level != skill.maxLevel)
-                slot[i].text.text = "LV. " + skill.level.ToString();
-            else
-                slot[i].text.text = "LV. " +
-                    "MAX";
-            i++;
+            if(data.type == SkillType.Active)
+            {
+                activeSlot[i].icon.sprite = data.icon;
+                if (skill.level != skill.maxLevel)
+                    activeSlot[i].text.text = "LV. " + skill.level.ToString();
+                else
+                    activeSlot[i].text.text = "LV. " +
+                        "MAX";
+                activeSlot[i].Show();
+                i++;
+            }
+            else if(data.type == SkillType.Passive)
+            {
+                passiveSlot[j].icon.sprite = data.icon;
+                if (skill.level != skill.maxLevel)
+                    passiveSlot[j].text.text = "LV. " + skill.level.ToString();
+                else
+                    passiveSlot[j].text.text = "LV. " +
+                        "MAX";
+                passiveSlot[j].Show();
+                j++;
+            }
+        }
+    }
+
+    public void HideAllSlot()
+    {
+        int activeCnt = activeSlot.Length;
+        int passiveCnt = passiveSlot.Length;
+        for(int i =0; i < activeCnt; i++)
+        {
+            activeSlot[i].Hide();
+        }
+        for(int i = 0; i < passiveCnt; i++)
+        {
+            passiveSlot[i].Hide();
         }
     }
 }
--- ProjectV/Assets/Scripts/UI/UI_StatusPanel.cs
+++ ProjectV/Assets/Scripts/UI/UI_StatusPanel.cs
@@ -6,13 +6,21 @@ public class UI_StatusPanel : UI
 {
     public static UI_StatusPanel instance;
 
-    [SerializeField] private RectTransform equipment;
+    private RectTransform rectTransform;
+    private RectTransform parent;
+    [SerializeField]
+    private RectTransform equipment;
     private UI_Equipment equipmentUI;
+
+    
     void Start()
     {
         instance = this;
 
         equipmentUI = equipment.GetComponent<UI_Equipment>();
+        rectTransform = GetComponent<RectTransform>();
+        parent = transform.parent.GetComponent<RectTransform>();
+        ResetSize();
     }
 
     public void SetSkillInfomations(List<Skill> skills)
@@ -20,4 +28,12 @@ public class UI_StatusPanel : UI
         equipmentUI.SetSkillInfomations(skills);
     }
 
+    public void ResetSize()
+    {
+        float width = parent.sizeDelta.x;
+        float height = parent.sizeDelta.y;
+        float equipmentPosY = height * -0.07f;
+        rectTransform.sizeDelta = new Vector2(width, height);
+        equipment.anchoredPosition = new Vector2(0f, equipmentPosY);
+    }
 }
