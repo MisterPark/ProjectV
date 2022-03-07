4fcdcd9af44d70e1271e2b7055edb6557f6fec69
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-03-07 16:51:54 +0900

    게임오브젝트삭제
    삭제,이관완료

--- ProjectV/Assets/Scripts/Skill/Skill_ForceFieldBarrier.cs
+++ ProjectV/Assets/Scripts/Skill/Skill_ForceFieldBarrier.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Skill_ForceFieldBarrier : Skill
 {
     GameObject obj;
-    DamageObject damageObject;
     protected override void Awake()
     {
         Kind = SkillKind.ForceFieldBarrier;
@@ -15,12 +14,31 @@ public class Skill_ForceFieldBarrier : Skill
     {
         Kind = SkillKind.ForceFieldBarrier;
         base.Start();
+        Unit unit = GetComponent<Unit>();
+        if (unit == null)
+        {
+            Debug.LogError("스킬을 유닛만 사용가능.");
+            return;
+        }
 
         obj = ObjectPool.Instance.Allocate("ForceFieldBarrier");
         obj.transform.position = Player.Instance.transform.position;
         obj.transform.forward = Player.Instance.transform.forward;
         obj.transform.SetParent(Player.Instance.transform);
-        damageObject = obj.GetComponent<DamageObject>();
+
+        Missile missile = obj.GetComponent<Missile>();
+        missile.Initialize();
+
+        missile.team = unit.team;
+        missile.owner = unit;
+        missile.duration = duration;
+        missile.damage = damage;
+        missile.speed = speed;
+        missile.delay = delay;
+        missile.type = MissileType.Guided;
+        missile.isPenetrate = true;
+        missile.OnCollision.RemoveAllListeners();
+        missile.OnCollision.AddListener(OnCollisionCallback);
         OnLevelUpCallback(1);
 
         OnLevelUp.AddListener(OnLevelUpCallback);
@@ -28,7 +46,6 @@ public class Skill_ForceFieldBarrier : Skill
 
     protected override void Active()
     {
-        damageObject.AttackFlag = true;
     }
 
     void OnLevelUpCallback(int nextLevel)
@@ -36,6 +53,11 @@ public class Skill_ForceFieldBarrier : Skill
         SkillData data = DataManager.Instance.skillDatas[(int)Kind].skillData;
         SkillLevel skillLevel = level.ToSkillLevel();
         SkillValue value = data.values[(int)skillLevel];
-        damageObject.damage = value.damage;
+        
+    }
+    void OnCollisionCallback(Vector3 pos)
+    {
+        //피격 이펙트
+
     }
 }
--- ProjectV/Assets/Scripts/Unit/Player/Player.cs
+++ ProjectV/Assets/Scripts/Unit/Player/Player.cs
@@ -57,14 +57,14 @@ public class Player : Unit
         OnLevelUp.AddListener(OnLevelUpCallback);
         UI_LevelUp.instance.OnSelected.AddListener(OnSelectSkill);
 
-        AddSkill(SkillKind.IceBolt);
+        //AddSkill(SkillKind.IceBolt);
         //AddSkill(SkillKind.FireBolt);
-        //AddSkill(SkillKind.ForceFieldBarrier);
+        AddSkill(SkillKind.ForceFieldBarrier);
         //AddSkill(SkillKind.BlackHole);
         //AddSkill(SkillKind.Laser);
         //AddSkill(SkillKind.Lightning);
 
-        AddSkill(SkillKind.FireTornado);
+        //AddSkill(SkillKind.FireTornado);
         //AddSkill(SkillKind.RockTotem);
         //AddSkill(SkillKind.ShurikenAttack);
         //AddSkill(SkillKind.Rain);
