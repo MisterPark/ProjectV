17e6d62a0430d0299bd0ae1c0d7334657cf350cc
djdngus32 <djdngus32@naver.com>
2022-03-08 17:20:30 +0900

    옵션 버튼연결 완료

--- ProjectV/Assets/Scripts/UI/UI_Option.cs
+++ ProjectV/Assets/Scripts/UI/UI_Option.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Option : MonoBehaviour
+public class UI_Option : UI
 {
+    private RectTransform rectTransform;
+    private RectTransform parent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        parent = transform.parent.GetComponent<RectTransform>();
+        ResetSize();
+        Hide();
+    }
+
+    public void OnClickExit()
+    {
+        Hide();
+    }
+
+    public void ResetSize()
+    {
+        float width = parent.sizeDelta.x;
+        float height = parent.sizeDelta.y;
+        float ratioX = 0.3f;
+        float ratioY = 0.8f;
+        rectTransform.sizeDelta = new Vector2(width * ratioX, height * ratioY);
     }
 }
