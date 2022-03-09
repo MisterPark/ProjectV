869e6cbcb1d531b3baba51059c1bc69e60955c92
wjrm600 <50776802+wjrm600@users.noreply.github.com>
2022-03-10 01:38:18 +0900

    stat panel powerup value change and text color

--- ProjectV/Assets/Scripts/UI/UI_CharacterSelectStatus.cs
+++ ProjectV/Assets/Scripts/UI/UI_CharacterSelectStatus.cs
@@ -20,20 +20,32 @@ public class UI_CharacterSelectStatus : MonoBehaviour
         playerStat = DataManager.Instance.playerCharacterData[(int)(DataManager.Instance.currentGameData.characterName)].statsData.stats;
         if (playerStat == null)
             return;
+
         foreach (UI_StatData child in children)
         {
             float tempStat = playerStat[(int)(child.statType)].origin_Stat;
-            float value = tempStat;
-            if (value == 0)
+            float tempPowerupStat = DataManager.Instance.powerUpStat[(int)(child.statType)];
+            float value = tempStat + tempPowerupStat;
+            if ((tempStat == 0) && (tempPowerupStat == 0))
                 child.value.text = "-";
             else
             {
+                if (tempPowerupStat != 0)
+                {
+                    child.value.color = Color.yellow;
+                }
+                else
+                {
+                    child.value.color = Color.black;
+                }
                 if (child.isPercent == false)
                 {
                     child.value.text = value.ToString();
                 }
                 else
+                {
                     child.value.text = value.ToString() + "%";
+                }
             }
 
         }
