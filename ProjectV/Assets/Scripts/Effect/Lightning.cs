68b18b0b1b688d1a76140983ed88cbca9364a436
MisterPark <40124101+MisterPark@users.noreply.github.com>
2022-03-14 16:14:27 +0900

    번개 수정

--- ProjectV/Assets/Scripts/Effect/Lightning.cs
+++ ProjectV/Assets/Scripts/Effect/Lightning.cs
@@ -7,10 +7,12 @@ public class Lightning : MonoBehaviour
     Camera cam;
     SphereCollider parentCollider;
     bool impactFlag = false;
+    Missile missile;
     void Start()
     {
         cam = Camera.main;
         parentCollider = gameObject.transform.GetComponentInParent<SphereCollider>();
+        missile = gameObject.GetComponentInParent<Missile>();
         transform.localScale = new Vector3(1, 0, 1);
         parentCollider.center = new Vector3(0, 20, 0);
     }
@@ -23,11 +25,19 @@ public class Lightning : MonoBehaviour
         impactFlag = false;
     }
 
+    private void OnDisable()
+    {
+        GameObject impact = ObjectPool.Instance.Allocate("LightningImpact");
+        ImpactV2 comp = impact.GetComponent<ImpactV2>();
+        comp.Duration = 0.3f;
+        impact.transform.position = transform.parent.transform.position;
+    }
+
     void FixedUpdate()
     {
         // 스케일 키우기
         float yScale = transform.localScale.y;
-        yScale +=  Time.fixedDeltaTime * 10f;
+        yScale += Time.fixedDeltaTime * 2 / missile.duration;
         transform.localScale = new Vector3(1, yScale, 1);
 
         // 콜라이더 위치 조정
@@ -38,13 +48,13 @@ public class Lightning : MonoBehaviour
         Vector3 camForward = new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z);
         transform.forward = camForward.normalized;
 
-        if(impactFlag ==false && yScale >= 2f)
+        if(impactFlag ==false && yScale >= 1.9f)
         {
-            impactFlag = true;
-            GameObject impact = ObjectPool.Instance.Allocate("LightningImpact");
-            ImpactV2 comp = impact.GetComponent<ImpactV2>();
-            comp.Duration = 0.3f;
-            impact.transform.position = transform.parent.transform.position;
+            //impactFlag = true;
+            //GameObject impact = ObjectPool.Instance.Allocate("LightningImpact");
+            //ImpactV2 comp = impact.GetComponent<ImpactV2>();
+            //comp.Duration = 0.3f;
+            //impact.transform.position = transform.parent.transform.position;
         }
     }
 }
