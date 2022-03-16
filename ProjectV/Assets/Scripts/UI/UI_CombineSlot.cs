c4a806e13df86cd21f05a94d0ae8f24c08ed428c
djdngus32 <djdngus32@naver.com>
2022-03-16 17:26:00 +0900

    스킬조합창 만들기 시작

---
+++ ProjectV/Assets/Scripts/UI/UI_CombineSlot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_CombineSlot : MonoBehaviour
+{
+    public Image skillA;
+    public Image skillB;
+    public Image skillC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color disableColor = new Color(0.3f, 0.3f, 0.3f, 0.9f);
+        Color ableColor = new Color(1f, 1f, 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
